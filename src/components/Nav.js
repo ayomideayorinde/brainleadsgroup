5545d55074c40ab4054663d31938ab4ba30979c9
ayomideayorinde <116303702+ayomideayorinde@users.noreply.github.com>
2025-06-17 18:02:50 +0100

    u

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,26 +2,27 @@ import logo from '../assets/brainleads.png'
 
 export default function Nav() {
     return (
-        <section className="fixed w-full z-10 top-0 left-0 py-3 px-[100px]">
-            <div className='flex items-center justify-between'>
-                <div className='flex items-center gap-2'>
+        <section className="fixed z-[999] top-5 left-0 py-2 px-[50px] mx-[50px] right-0 rounded-full">
+            <div className='flex items-center justify-between '>
+                <a href='/' className='flex items-center gap-2 z-[999]'>
                     <img src={logo} className='w-[40px]'/>
                     <p>Brainleads</p>
-                </div>
-                <div>
-                    <ul className='flex items-center gap-10'>
-                        <li className='cursor-pointer hover:text-[#FFC734] hover:border-b-2 border-[#FFC734]'><a>Home</a></li>
-                        <li className='cursor-pointer hover:text-[#FFC734] hover:border-b-2 border-[#FFC734]'><a>Services</a></li>
-                        <li className='cursor-pointer hover:text-[#FFC734] hover:border-b-2 border-[#FFC734]'><a>Benefits</a></li>
-                        <li className='cursor-pointer hover:text-[#FFC734] hover:border-b-2 border-[#FFC734]'><a>Contact</a></li>
+                </a>
+                <div className='z-[999]'>
+                    <ul className='flex items-center gap-5 '>
+                        <li className='cursor-pointer hover:bg-[#FFC734] px-4 py-1 rounded-lg'><a>Home</a></li>
+                        <li className='cursor-pointer hover:bg-[#FFC734] px-4 py-1 rounded-lg'><a>Services</a></li>
+                        <li className='cursor-pointer hover:bg-[#FFC734] px-4 py-1 rounded-lg'><a>Benefits</a></li>
+                        <li className='cursor-pointer hover:bg-[#FFC734] px-4 py-1 rounded-lg'><a>Contact</a></li>
                     </ul>
                 </div>
-                <div>
-                    <button className='bg-[#FFC734] px-4 py-1 rounded-md text-black font-semibold hover:bg-[#FFB000] transition-colors duration-300'>
+                <div className='z-[999]'>
+                    <button className='primary-bg px-4 py-1 rounded-full font-semibold transition-colors duration-300'>
                         Sign Up
                     </button>
                 </div>
             </div>
+            <div className='bg-white opacity-50 w-full h-full absolute top-0 left-0 right-0 rounded-full'></div>
         </section>
     )
 }
