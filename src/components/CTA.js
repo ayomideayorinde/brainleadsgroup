713878d6f28f3b453ce4bfe8fc857356ca1d4e92
ayomideayorinde <116303702+ayomideayorinde@users.noreply.github.com>
2025-06-18 09:01:59 +0100

    u

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -2,10 +2,13 @@
 export default function CTA() {
   return (
     <section className="bg-[#333333] px-[25px] py-10 lg:px-[50px]">
-      <div className="flex flex-col items-center gap-10 lg:px-[50px] px-[25px] py-10">
-        <h1 className="text-center text-white text-3xl lg:text-4xl font-bold">
+      <div className="flex flex-col items-center gap-8 lg:px-[50px] px-[25px] py-10">
+        <h1 className="text-center text-white text-2xl lg:text-3xl font-bold capitalize">
           Digital marketing connects your business to the customers looking for you.
         </h1>
+        <p className="text-center text-white text-md lg:text-xl font-normal">
+          We create a strategy that binds you up with your target audience, driving not just traffic flow but conversions.
+        </p>
         <button className='rounded-full font-bold primary-bg px-5 py-3'>Book A Free Consultation</button>
       </div>
     </section>
--- src/components/Services.js
+++ src/components/Services.js
@@ -3,10 +3,14 @@ import { faNetworkWired, faChartColumn, faCalendar, faDashboard } from '@fortawe
 // import { faBar } from '@fortawesome/free-brands-svg-icons'
 
 const serviceCard = [
-  {icon:faChartColumn, title:'Media Strategy & Planning', desc:'We use real insights to guide every step.'},
-  {icon:faNetworkWired, title:'Full-Funnel Media Activation', desc:'We connect at every customer touchpoint.'},
-  {icon:faCalendar, title:'Media Operations', desc:'We make sure everything runs on track.'},
-  {icon:faDashboard, title:'Data, Analytics & Technology', desc:'You get full visibility in real time.'}
+  {icon:faChartColumn, title:'Media Strategy & Planning', 
+   desc:'Informed by data-driven audience insights, we curate the best strategic plan for both your brand, and your audience.'},
+  {icon:faNetworkWired, title:'Full-Funnel Media Activation', 
+   desc:'Activating across the media of today and tomorrow, we adapt to the ever-changing behaviors of people with tactics designed to drive connection with your customers.'},
+  {icon:faCalendar, title:'Media Operations', 
+  desc:"Crossing the t's and dotting the i's, our project management and ad operations teams ensure smooth execution from onboarding to campaign wrap-up and everything in between."},
+  {icon:faDashboard, title:'Data, Analytics & Technology', 
+   desc:'Collaboratively built dashboards mean that you have access to clear campaign reporting every step of the way, because we know transparency matters.'}
 ]
 
 export default function Services() {
@@ -15,7 +19,7 @@ export default function Services() {
       <div className="text-center py-12">
         <h1 className="text-3xl">Comprehensive Marketing Strategy</h1>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 p-6">
         {serviceCard.map((service, index) => (
           <div key={index} className="shadow-lg rounded-lg p-4 hover:shadow-2xl transition-shadow duration-300 card-bg" data-aos="fade-up" data-aos-delay={index * 100}>
             <div className="flex items-center justify-center mb-4">
