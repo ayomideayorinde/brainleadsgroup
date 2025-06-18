7f699651340af06147288bf5e79b64618dc00d95
ayomideayorinde <116303702+ayomideayorinde@users.noreply.github.com>
2025-06-18 20:41:04 +0100

    u

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -3,10 +3,10 @@ export default function CTA() {
   return (
     <section className="bg-[#333333] px-[25px] py-10 lg:px-[50px]">
       <div className="flex flex-col items-center gap-8 lg:px-[50px] px-[25px] py-10">
-        <h1 className="text-center text-white text-2xl lg:text-3xl font-bold capitalize">
+        <h1 className="text-center text-white text-[32px] lg:text-[40px] font-bold capitalize">
           Digital marketing connects your business to the customers looking for you.
         </h1>
-        <p className="text-center text-white text-md lg:text-xl font-normal">
+        <p className="text-center text-white text-[18px] lg:text-[20px] font-normal">
           We create a strategy that binds you up with your target audience, driving not just traffic flow but conversions.
         </p>
         <button className='rounded-full font-bold primary-bg px-5 py-3'>Book A Free Consultation</button>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,10 +8,10 @@ export default function Hero() {
       </div>
       <div className='bg-black w-full h-full absolute z-10 top-0 right-0 left-0 opacity-50'></div>
       <div className='w-full flex flex-col items-center gap-[20px] lg:px-[150px] px-[50px] absolute z-20 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-white text-center'>
-        <p className='text-2xl lg:text-4xl font-bold capitalize'>
+        <p className='text-[32px] lg:text-[40px] font-bold capitalize'>
             Rise above the noise. <span className='primary-fg'>Discover what’s true.</span>
         </p>
-        <p className='text-md lg:text-xl font-semibold'>
+        <p className='text-[18px] lg:text-[20px]'>
             Your business deserves to prosper. Whether local or global, we create your path through digital marketing.
         </p>
         <button className='rounded-full font-bold primary-bg px-5 py-3'>Book A Free Consultation</button>
