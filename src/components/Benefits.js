bdbde7a90324b87eb0e13e0dd93b15c0ef45c6ad
ayomideayorinde <116303702+ayomideayorinde@users.noreply.github.com>
2025-06-18 20:44:15 +0100

    Update Benefits.js

--- src/components/Benefits.js
+++ src/components/Benefits.js
@@ -24,7 +24,7 @@ const benefits = [
 
 export default function Benefits() {
     return (
-        <section className="bg-[#F8F9FC] px-[25px] py-5 lg:px-[50px]">
+        <section className="bg-[#F8F9FC] py-5 lg:px-[50px]">
             <div className="text-center py-12 px-[25px] lg:px-[50px]">
                 <h1 className="text-[32px] lg:text-[40px] font-bold">
                     The Benefits of Digital Marketing to Your Business
