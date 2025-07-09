6837ce46083e64cfb6790366ececbff07fe693d7
ayomideayorinde <116303702+ayomideayorinde@users.noreply.github.com>
2025-07-09 23:18:40 +0100

    Update Services.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,8 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faNetworkWired, faChartColumn, faCalendar, faDashboard } from '@fortawesome/free-solid-svg-icons'
+import { faNetworkWired, faChartColumn, faCalendar, faDashboard, faVideoCamera } from '@fortawesome/free-solid-svg-icons'
 // import { faBar } from '@fortawesome/free-brands-svg-icons'
 
 const serviceCard = [
+  {icon:faVideoCamera, title:'Content Video Production', 
+   desc:'With our production experts, we produce high-quality video content specifically designed for monetization and sales conversion.'},
   {icon:faChartColumn, title:'Media Strategy & Planning', 
    desc:'Informed by data-driven audience insights, we curate the best strategic plan for both your brand, and your audience.'},
   {icon:faNetworkWired, title:'Full-Funnel Media Activation', 
@@ -20,7 +22,7 @@ export default function Services() {
         <h1 className="text-[32px] lg:text-[40px] font-bold">Comprehensive Marketing Strategy</h1>
         <h2 className='text-2xl primary-fg bg-[#333333] px-5 py-1 text-center rounded-full'>What We Offer</h2>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
         {serviceCard.map((service, index) => (
           <div key={index} className="flex flex-col items-start lg:items-center shadow-lg rounded-lg p-4 hover:shadow-2xl transition-shadow duration-300 card-bg" data-aos="fade-up" data-aos-delay={index * 100}>
             <div className="flex items-center justify-center mb-4">
