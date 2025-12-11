4838fd4f0e2468e1a8978dc9eb5a1e1d4f29e820
ayomideayorinde <116303702+ayomideayorinde@users.noreply.github.com>
2025-12-11 12:01:41 +0100

    Comment out ErrorGate function and blocking logic
    Comment out the ErrorGate function and related logic in App component.

--- src/App.js
+++ src/App.js
@@ -15,22 +15,22 @@ import SEO from "./components/SEO.js";
 
 
 
-function ErrorGate() {
-  return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.8 }}
-      className="fixed inset-0 flex flex-col justify-center items-center bg-gray-900 text-white z-[2000] px-6 text-center"
-    >
-        <h1 className="lg:text-8xl text-6xl mb-4">⚠️</h1>
-      <h1 className="text-4xl font-bold mb-4">Something Went Wrong</h1>
-      <p className="text-lg">
-        The application is currently unavailable. Please check back later.
-      </p>
-    </motion.div>
-  );
-}
+// function ErrorGate() {
+//   return (
+//     <motion.div
+//       initial={{ opacity: 0 }}
+//       animate={{ opacity: 1 }}
+//       transition={{ duration: 0.8 }}
+//       className="fixed inset-0 flex flex-col justify-center items-center bg-gray-900 text-white z-[2000] px-6 text-center"
+//     >
+//         <h1 className="lg:text-8xl text-6xl mb-4">⚠️</h1>
+//       <h1 className="text-4xl font-bold mb-4">Something Went Wrong</h1>
+//       <p className="text-lg">
+//         The application is currently unavailable. Please check back later.
+//       </p>
+//     </motion.div>
+//   );
+// }
 
 const orgLd = {
   "@context": "https://schema.org",
@@ -62,23 +62,23 @@ const orgLd = {
 };
 
 function App() {
-  const [blocked, setBlocked] = useState(false);
+  // const [blocked, setBlocked] = useState(false);
 
-  useEffect(() => {
-    AOS.init({
-      duration: 1000,
-      once: true,
-    });
+  // useEffect(() => {
+  //   AOS.init({
+  //     duration: 1000,
+  //     once: true,
+  //   });
 
     
-    const stopDate = new Date("2050-10-06T23:59:59");
-    const now = new Date();
-    if (now > stopDate) {
-      setBlocked(true);
-    }
-  }, []);
+  //   const stopDate = new Date("2050-10-06T23:59:59");
+  //   const now = new Date();
+  //   if (now > stopDate) {
+  //     setBlocked(true);
+  //   }
+  // }, []);
 
-  if (blocked) return <ErrorGate />;
+  // if (blocked) return <ErrorGate />;
 
   return (
     <div className="bg-gray-50 text-gray-800 font-sans">
